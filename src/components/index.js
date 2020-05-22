8a7c4c7986d23c9f79c53be6a4723bac75304ea0
Ehsaan Ali <38082320+EhsaanAli97@users.noreply.github.com>
2020-05-22 13:19:16 +0100

    COVID-19 App

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+//Takes all the components and exports it from the component folder
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
